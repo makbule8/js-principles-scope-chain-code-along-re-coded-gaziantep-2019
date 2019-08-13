b4ce2c3d4093269603dd3ad3a9f3243b650960a8
makbule özbay <makbuleozbay8@gmail.com>
2019-08-13 13:53:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 /* Your code goes here */
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'binnur';
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'not binnur';
+}
+ //function attemptTwoFavoriteCustomers() {
+ // favoriteCustomer = 'binnur';
+//let favoriteCustomer = 'not binnur';
+//}
